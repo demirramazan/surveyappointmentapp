33ee57e419342f3b997fbef06cae91e56e6a675f
rdemir <ramazan.demir@mebitech.com>
2017-12-22 17:57:37 +0300

    duzenleme

---
+++ src/main/java/com/rdemir/donemprojesi/controllers/MenuController.java
@@ -0,0 +1,9 @@
+package com.rdemir.donemprojesi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+}
