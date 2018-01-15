06d0b96a251537cc0f069faa1cd713177de981ff
ramazan <rdemir4434@gmail.com>
2018-01-16 01:32:09 +0300

    düzenlee

--- src/main/java/com/rdemir/donemprojesi/controllers/UserController.java
+++ src/main/java/com/rdemir/donemprojesi/controllers/UserController.java
@@ -4,9 +4,14 @@ import com.rdemir.donemprojesi.entities.Role;
 import com.rdemir.donemprojesi.entities.User;
 import com.rdemir.donemprojesi.interfaces.services.IUserService;
 import com.rdemir.donemprojesi.scope.ScopeName;
+import org.primefaces.event.SelectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import java.util.List;
 
 @Component("userBean")
 @Scope(ScopeName.SESSION)
@@ -16,12 +21,19 @@ public class UserController {
     private IUserService userService;
 
     private User user;
+    private User selectedUser;
+    private List<User> userList;
     private String username;
     private String password;
     private String name;
     private String surname;
     private Boolean enabled;
     private Role role;
+
+    @PostConstruct
+    public void init() {
+        userList = userService.getAllUsers();
+    }
 
     public UserController() {
         user = new User(username, password, name, surname, enabled, role, null);
@@ -98,4 +110,25 @@ public class UserController {
     public void setEnabled(Boolean enabled) {
         this.enabled = enabled;
     }
+
+    public User getSelectedUser() {
+        return selectedUser;
+    }
+
+    public void setSelectedUser(User selectedUser) {
+        this.selectedUser = selectedUser;
+    }
+
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public void onRowSelect(SelectEvent event) {
+        FacesContext.getCurrentInstance().addMessage(null, null);
+    }
+
 }
